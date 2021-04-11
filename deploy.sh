7db8a0e4b9acefca61cf7b9be14eff5bbf1cdbb3
valendre <valendre@gmail.com>
2021-04-11 21:57:35 +0200

    scripts

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,8 @@ sudo git pull origin main
 sudo npm install
 cd client
 npm install
+sudo chown -R $USER:$USER /home/ubuntu/mern-app/client
+chmod -R 777 /home/ubuntu/mern-app/client/build
 sudo npm run-script build
 cd ..
 sudo systemctl restart nginx
