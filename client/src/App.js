30d3e6b119d3c5d3b61b3d2c8da2a2ebe9cb0cd2
valendre <valendre@gmail.com>
2021-04-11 20:35:54 +0200

    App.js cleaining

--- client/src/App.js
+++ client/src/App.js
@@ -1,44 +1,29 @@
 import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
     const [response, setResponse] = useState('...')
     useEffect(() =>{
         async function checkAPIRoute() {
             try {
-                const host = process.env.REACT_APP_HOST || "http://localhost:5000"
+                const host = process.env.REACT_APP_HOST"
                 const response = await fetch(
                     `${host}/api`
                 )
                 if (response.status === 200) {
-                    setResponse("a Success!!!")
+                    setResponse(response.toString)
                 }
             } catch (err) {
-                setResponse("Oops, something went wrong...")
+                setResponse(err.toString)
             }
         }
         checkAPIRoute()
     }, [response])
 
   return (
-    <div className="App">
-      <header className="App-header">
+    <div>
         <h1>Mern-app</h1>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-        </a>
-        <h1>The API call is...</h1>
-          <h2>{response}</h2>
-      </header>
+        <h2>The API call is...</h2>
+        <p>{response}</p>
     </div>
   );
 }
