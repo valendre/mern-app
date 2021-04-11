bbe9536a0e69a745c7afedc09e8d146a988a96e4
valendre <valendre@gmail.com>
2021-04-11 22:00:03 +0200

    scripts

--- deploy.sh
+++ deploy.sh
@@ -7,5 +7,6 @@ sudo chown -R $USER:$USER /home/ubuntu/mern-app/client
 chmod -R 777 /home/ubuntu/mern-app/client/build
 sudo npm run-script build
 cd ..
+
 sudo systemctl restart nginx
 sudo pm2 restart all
